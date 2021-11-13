77f585e6a89dc0e35d5df30c08a7eca7f738afc5
João Vítor <joaovsoutosantos@gmail.com>
2021-11-13 13:28:23 -0300

    style: create global styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import { GlobalStyles } from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and styled-components"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Main } from 'components/Main'
+
 export default function Home() {
-  return <h1>hello world</h1>
+  return <Main />
 }
