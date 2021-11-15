78caa0269e68191c62a6e0df7f8bcf19f5d95304
João Vítor <joaovsoutosantos@gmail.com>
2021-11-15 19:34:24 -0300

    chore: remove addon-knobs

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,22 +1,10 @@
 import type { Meta, Story } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 import { Main } from '.'
 
-const sbConfig = {
+export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export default sbConfig
-
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'Typescript, ReactJS, Next.js e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = args => <Main {...args} />
