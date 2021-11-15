bb17f7da1f620eb0e7bd337783a4b4e7abc2e050
João Vítor <joaovsoutosantos@gmail.com>
2021-11-15 20:00:36 -0300

    chore: configure next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ function App({ Component, pageProps }: AppProps) {
 
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
 
         <meta
           name="description"
